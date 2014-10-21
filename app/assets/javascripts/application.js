0a2830fb4773ca5de9d45199d7a25f7c435ca290
Angela Chen <chen.angelay@gmail.com>
2014-10-21 14:27:59 -0700

    added streaming cats

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,38 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+// Declare the variables we will be using
+var slides, slides_total, slide_current;
+
+//receives the number and hides images and only displays that image number
+function changePicture (slide) {
+	for(var i=0; i<slides_total; i++){
+	slides[i].style.display='none';
+	}
+	slides[slide].style.display='block';
+};
+
+
+// Create an event listener to make sure code will execute after HTML is available
+document.addEventListener('DOMContentLoaded', function(){
+	slides = document.getElementsByClassName('col-sm-12');
+	slides_total=slides.length;
+	slide_current=0;
+	changePicture(1);
+
+	// set interval to call function every x milliseconds
+	window.setInterval(function() {
+		// if reach end of slides, reset
+		if(slide_current >= (slides_total -1)){
+			slide_current=0;
+		// otherwise go to next slide
+		} else {
+			slide_current++;
+		}
+		changePicture(slide_current);
+	},3000);
+});
